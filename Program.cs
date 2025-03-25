4d75fdbf31fea3df1d6a3fce7c5eee2765f2268a
DjDino420 <bmb.petyx@gmail.com>
2025-03-25 11:01:36 +0200

    working project

--- Program.cs
+++ Program.cs
@@ -3,49 +3,42 @@ using CodespringProject.Repositories;
 using CodespringProject.Repositories.Interfaces;
 using CodespringProject.Services;
 using CodespringProject.Services.Implementations;
+using Microsoft.AspNetCore.Connections;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Read Connection String from Environment Variable or appsettings.json
-var connectionString = Environment.GetEnvironmentVariable("AZURE_MYSQL_CONNECTIONSTRING")
-                       ?? builder.Configuration.GetConnectionString("MySqlConnection");
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = Environment.GetEnvironmentVariable("MYSQL_CONNECTIONSTRING")
+                       ?? throw new ConnectionAbortedException("cannot retrive connection string from environment varriable");
+Console.WriteLine($"Connection String: {connectionString}");
+
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
 );
 
-// Add Services & Repositories
 builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
 builder.Services.AddScoped<IRecipeService, RecipeService>();
-builder.Services.AddScoped<IIngredientService, IngredientService>();
+
 builder.Services.AddScoped<IIngredientRepository, IngredientRepository>();
-
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-        options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
-    });
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IIngredientService, IngredientService>();
 
 var app = builder.Build();
 
-// Automatically Apply Migrations on Startup
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     dbContext.Database.Migrate();
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
 app.MapControllers();
+
 app.Run();
